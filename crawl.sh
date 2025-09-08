a545ae33f7dd688ac0fb1555156e91881b87f93d
cceayhw <cceayhw@ucl.ac.uk>
2025-09-08 13:04:49 +0100

    .

--- crawl.sh
+++ crawl.sh
@@ -3,33 +3,39 @@
 URL=$1
 DEPTH="${2:-2}"
 
-          EX_FILES='.(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|js)'
-          time $HOME/go/bin/gospider -d ${DEPTH} -c 10 --blacklist "${EX_FILES}" -s ${URL} |\
-            tr -d '\r' |\
-            grep -v -E '\[(form|subdomains|linkfinder|javascript)\]' |\
-            grep -v -E '.(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|js)$' |\
-            grep -v -E '.(js|css)?ver=//g' | grep -v '\[code-40?\]'|\
-            sed -e 's/^.* - //g' -e 's/http:/https:/g' -e 's/\/$//g' |\
-            grep -v '^mailto:' | grep "^${URL}" |\
-            grep -v wp-json/oembed |\
-            sort -u > gospider.out
+EX_FILES='.(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|js)'
+time $HOME/go/bin/gospider -d ${DEPTH} -c 10 --blacklist "${EX_FILES}" -s ${URL} |\
+  tr -d '\r' |\
+  grep -v -E '\[(form|subdomains|linkfinder|javascript)\]' |\
+  grep -v -E '.(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|js)$' |\
+  grep -v -E '.(js|css)?ver=//g' | grep -v '\[code-40?\]'|\
+  sed -e 's/^.* - //g' -e 's/http:/https:/g' -e 's/\/$//g' |\
+  grep -v '^mailto:' | grep "^${URL}" |\
+  grep -v wp-json/oembed |\
+  sort -u > gospider.out
 
-          export ENTRIES=$(wc -l gospider.out | cut -d ' ' -f1)
-          echo "# entries: ${ENTRIES}"
-          echo "(l(${ENTRIES})/l(50))+1" | bc -l 
-          BIG_O=$(echo "(l(${ENTRIES})/l(50))+1" | bc -l | sed 's/\.[0-9]*$//g')
-          echo $BIG_O
-          SIZE=L
-          case "${BIG_O}" in
-            "0")
-              SIZE= ;;
-            "1")
-              SIZE=S ;;
-            "2" | "3" )
-              SIZE=M ;;
-            *)
-              SIZE=L ;;
-          esac
+export ENTRIES=$(wc -l gospider.out | cut -d ' ' -f1)
+
+echo "# entries: ${ENTRIES}"
+echo "(l(${ENTRIES})/l(50))+1" | bc -l 
+
+BIG_O=$(echo "(l(${ENTRIES})/l(50))+1" | bc -l | sed 's/\.[0-9]*$//g')
+echo $BIG_O
+
+SIZE=L
+case "${BIG_O}" in
+  "0")
+    SIZE= ;;
+  "1")
+    SIZE=S ;;
+  "2" | "3" )
+    SIZE=M ;;
+  *)
+    SIZE=L ;;
+esac
+
+MATRIX_N=$(printf "%s\n" {a..z} | head -"${BIG_O}" | jq -Rsc '. / "\n" - [""]')
+
 
 TMP_FILE=tmp.%%
 if [ "${CI}" != "true" ]
@@ -40,10 +46,11 @@ fi
           echo "entries=${ENTRIES}" >> "$GITHUB_OUTPUT"
           echo "big_o=${BIG_O}" >> "$GITHUB_OUTPUT"
           echo "size=${SIZE}" >> "$GITHUB_OUTPUT"
-          #seq \{1..${BIG_O}\}
+          echo "matrix_n=${MATRIX_N}}" >> "$GITHUB_OUTPUT"
 
           echo "### URL: [${URL}] crawl depth: [${DEPTH}]" >> $GITHUB_STEP_SUMMARY
           echo "### Crawled page count: [${ENTRIES}]" >> $GITHUB_STEP_SUMMARY
           echo "### Big O: [${BIG_O}] size is [${SIZE}]" >> $GITHUB_STEP_SUMMARY
+          echo "### Matrix sizes: [${MATRIX_N}]" >> $GITHUB_STEP_SUMMARY
 
 [ "${CI}" != "true" ] && cat "${TMP_FILE}" ; rm -f "${TMP_FILE}"
